353709cd153ea7a60232cbfb2c31780c60082190
Abraham Park <apark064@ucr.edu>
2020-10-14 16:57:51 -0700

    added html pages

--- mysite/fifa/urls.py
+++ mysite/fifa/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #url(r'^admin/', admin.site.urls),
+    path('homepage/', views.homepage, name='homepage'),
+    #url(r'^homepage/', views.homepage),
 ]
--- mysite/fifa/views.py
+++ mysite/fifa/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. This is fifa app.")
+
+def homepage(request):
+#    return HttpResponse('homepage')
+     return render(request, 'homepage.html')
