94509fe7f507cd8b7cc418382e421b27c1d87d05
mustafamanya <mustafamanya@gmail.com>
2020-10-14 20:01:49 -0700

    output
    output name to console

--- mysite/fifa/views.py
+++ mysite/fifa/views.py
@@ -5,6 +5,17 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. This is fifa app.")
 
+#def homepage(request):
+
+ #    return render(request, 'homepage.html')
+
 def homepage(request):
-#    return HttpResponse('homepage')
+
+     print(request.POST)
      return render(request, 'homepage.html')
+
+def homepage(request):
+     print(request.GET)
+     return render(request, 'homepage.html')
+
+
