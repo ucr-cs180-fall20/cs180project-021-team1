c063a534db7e19e6b207f9dee08addad3b960688
Calvin <tng1014>
2020-10-19 15:33:04 -0700

    search bar.
    restart whole new page and make a search bar.

--- mysite/fifa/views.py
+++ mysite/fifa/views.py
@@ -5,19 +5,18 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. This is fifa app.")
 
-#def homepage(request):
-
- #    return render(request, 'homepage.html')
-
 def homepage(request):
+    return render(request, 'homepage.html')
 
-     print(request.POST)
-     return render(request, 'homepage.html')
-
-def homepage(request):
-     print(request.GET)
-     print('Your request has been recieved ')
-     return render(request, 'homepage.html')
+# def homepage(request):
+#
+#      print(request.POST)
+#      return render(request, 'homepage.html')
+#
+# def homepage(request):
+#      print(request.GET)
+#      print('Your request has been recieved ')
+#      return render(request, 'homepage.html')
 
 
 
