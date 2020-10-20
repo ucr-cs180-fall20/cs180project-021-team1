0fecf76a7abaed6634aa1de34c24a89837f18399
mustafamanya <mustafamanya@gmail.com>
2020-10-19 18:12:02 -0700

    added get function
    added a get function that can be used to search database

--- mysite/fifa/views.py
+++ mysite/fifa/views.py
@@ -5,18 +5,21 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. This is fifa app.")
 
-def homepage(request):
-    return render(request, 'homepage.html')
+# def homepage(request):
+#     return render(request, 'homepage.html')
 
 # def homepage(request):
-#
+#      playerName=request.POST # playerName is the variable saved once searched. Use this to search Database.
 #      print(request.POST)
+#      print(playerName)
 #      return render(request, 'homepage.html')
 #
-# def homepage(request):
-#      print(request.GET)
-#      print('Your request has been recieved ')
-#      return render(request, 'homepage.html')
+def homepage(request):
+     playerName = request.GET #playerName is the variable saved once searched. Use this to search Database.
+     print(request.GET)
+     print(playerName)
+     print('Your request has been recieved ')
+     return render(request, 'homepage.html')
 
 
 
