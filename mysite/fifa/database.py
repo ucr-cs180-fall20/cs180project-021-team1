a3b9a4fdd2868b50ebad46a1603a923c6cb9fb1e
Abraham Park <apark064@ucr.edu>
2020-10-24 16:40:43 -0700

    initial database class

---
+++ mysite/fifa/database.py
@@ -0,0 +1,17 @@
+#basic imports
+import pandas as pd
+
+class database:
+    
+    def __init__:
+
+    def readFile(file):
+        f = open(file, 'r', encoding='utf-8')
+
+    def searchEntry():
+
+    def addEntry():
+
+    def modifyEntry():
+
+    def deleteEntry():
