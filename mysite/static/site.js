a374a023c485ab04d7116786c8667fcbbb04b3e9
Calvin Ng <calvin32897@gmail.com>
2020-11-12 04:45:06 -0800

    logic is done. but coun't fix the main error

--- mysite/static/site.js
+++ mysite/static/site.js
@@ -1,5 +1,48 @@
-{/* <script>
-    function helloWorld() {
-        alert("HelloWorld")
-    }
-</script> */}
+
+// const form = document.getElementById('form');
+// const playerName_input = document.getElementById('playerName').value;
+
+// form.addEventListener('submit', e => {
+// 	e.preventDefault();
+//
+// 	checkValid();
+// });
+// function returnToPreviousPage() {
+//     window.history.back();
+// }
+
+function checkValid(e) {
+      e.preventDefault();
+      const playerName_input = document.getElementById('playerName').value;
+
+      var letterOnly = /^[a-zA-z]+$/;
+      var numberonly = /^[0-9]+$/;
+      var bothNumLet = /^[0-9a-zA-Z]+$/;
+      var positionOnly = /^[A-Za-z]+$/;
+
+      // if (playerName_input == letterOnly){
+      // if (playerName_input.match(letterOnly)) {
+      if (letterOnly.test(playerName_input)== false) {
+            window.alert('Please input only alphabet characters only.');
+            playerName_input.focus();
+            // e.preventDefault();
+            return false;
+      }
+      // alert('Your name have accepted : you can try another')
+            console.log("hello world");
+            return true;
+}
+//
+// $(function() {
+//       $('#form').on('submit', function(event) {
+//             if (!isValid()) {
+//                  event.preventDefault();
+//             }
+//
+//       })
+// })
+
+      // alert('Please input only alphabet characters only.');
+      // playerName_input.focus();
+      //       return false;
+
