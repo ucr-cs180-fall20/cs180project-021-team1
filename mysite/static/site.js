6ebb1f3f050b6f35a9ad27f621350cd76d80819e
Calvin Ng <calvin32897@gmail.com>
2020-11-16 01:50:33 -0800

    validation for search form is done

--- mysite/static/site.js
+++ mysite/static/site.js
@@ -1,15 +1,3 @@
-// const form = document.getElementById('form');
-// const playerName_input = document.getElementById('playerName').value;
-
-// form.addEventListener('submit', e => {
-// 	e.preventDefault();
-//
-// 	checkValid();
-// });
-// function returnToPreviousPage() {
-//     window.history.back();
-// }
-
 function checkValid() {
       // e.preventDefault();
       // var playerName_input = document.getElementById('playerName').value;
@@ -20,14 +8,13 @@ function checkValid() {
       var ratingValue = document.forms["addForm"]["playerRating"].value;
       var positionValue = document.forms["addForm"]["playerPosition"].value;
       var potentialValue = document.forms["addForm"]["playerPotential"].value;
-      // var idValue = document.forms["addForm"]["playerId"].value;
+      var idValue = document.forms["addForm"]["playerId"].value;
 
       var letterOnly = /^[a-zA-Z\s]+$/;
       var numberOnly = /^[0-9]+$/;
       var bothlet = /^[0-9a-zA-Z\s]+$/;
-      var positionOnly = /^[A-Za-z|\s]+$/;
+      var positionOnly = /^[A-Za-z|]+$/;
       // console.log("age value length is : " + ageValue.length);
-      console.log("age value is : " + ageValue);
 
       if (letterOnly.test(playerValue) == false) {
             console.log("player name test is working!!!!!!");
@@ -86,4 +73,67 @@ function checkValid() {
       console.log("hello world");
       return true;
 
+}
+
+function searchValid() {
+      var letterOnly = /^[a-zA-Z\s]+$/;
+      var numberOnly = /^[0-9]+$/;
+      var bothlet = /^[0-9a-zA-Z\s]+$/;
+      var positionOnly = /^[A-Za-z|]+$/;
+
+      // var checkDropdown = document.getElementById("dropdown").value;
+      var checkDropdown = document.forms["searchForm"]["dropdown"].value;
+      // var dropdown1 = checkDropdown.options[checkDropdown.player_name].value;
+      var inputValue = document.forms["searchForm"]["name"].value;
+      if (checkDropdown == "player_name" && letterOnly.test(inputValue) == false){
+            // letterOnly.test(inputValue) == false
+            console.log("player name test is working!!!!!!");
+            window.alert('Please input alphabet characters only for player name.');
+            // document.getElementById('playerName').focus();
+            return false;
+      }
+
+      if (checkDropdown == "age" && (numberOnly.test(inputValue) == false || inputValue < 1 || inputValue > 99)){
+            console.log(" age test is working ");
+            console.log("age value is : " + inputValue);
+            window.alert('Please input a vaild age between 1 to 99');
+            // document.getElementById('playerAge').focus();
+            return false;
+      }
+
+      if (checkDropdown == "nationality" && letterOnly.test(inputValue) == false){
+            // letterOnly.test(inputValue) == false
+            console.log("nationality test is working!!!!!!");
+            window.alert('Please input alphabet characters only for nationality.');
+            // document.getElementById('playerName').focus();
+            return false;
+      }
+
+      if (checkDropdown == "club" &&  bothlet.test(inputValue) == false) {
+            console.log("club test is working!!!!!!");
+            window.alert('Please input alphabet characters and number only for club name. ');
+            // document.getElementById('clubName').focus();
+            return false;
+      }
+
+      if (checkDropdown == "rating" && (numberOnly.test(inputValue) == false || inputValue < 1 || inputValue > 99)){
+            console.log(" rating test is working ");
+            console.log("rating value is : " + inputValue);
+            window.alert('Please input a vaild rating between 1 to 99');
+            // document.getElementById('playerAge').focus();
+            return false;
+      }
+
+      if (checkDropdown == "position" && positionOnly.test(inputValue) == false){
+            // letterOnly.test(inputValue) == false
+            console.log("position test is working!!!!!!");
+            window.alert('Please input alphabet characters only for position and add " | " for multiple positions without spaces.');
+            // document.getElementById('playerName').focus();
+            return false;
+      }
+
+
+
+      console.log("hello world");
+      return true;
 }
