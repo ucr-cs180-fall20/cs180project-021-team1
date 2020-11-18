55153cd6b6a02066544578490ea2d51779ae7882
hodafarhat <hfarh002@ucr.edu>
2020-11-18 01:57:38 -0800

    soccerballs on field, drawer replaces navbar

--- mysite/static/field.js
+++ mysite/static/field.js
@@ -49,11 +49,3 @@ function draw() {
 
 
 }
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
